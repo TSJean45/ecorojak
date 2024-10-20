77cd17dc1b3906046e793f14997b2dd414f9c7cb
TSJean45 <szujean45@gmail.com>
2024-10-21 03:33:36 +0800

    Fixed incorrect babel config

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,7 @@
-module.exports = {
-  content: [
-    './App.{js,jsx,ts,tsx}', 
-    './app/**/*.{js,jsx,ts,tsx}', 
-    './components/**/*.{js,jsx,ts,tsx}',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: ["nativewind/babel"],
+  };
 };
